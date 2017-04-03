3ccda98707c49448462e3a9a9fa863115c6eeda4
flext0r <maxxv100@gmail.com>
2017-04-03 18:20:43 +0200

    Revert "v0.4B"
    This reverts commit 9c14687dbc40e062669665935085a4c1beadb8ea.

---
+++ silnia.php
@@ -0,0 +1,34 @@
+<?php
+require_once 'system/templates/default/layout.php';
+$result = null;
+if(isset($_POST['Send']))
+{
+	$result = $Calculator->Silnia($_POST['N']);
+}
+
+startblock('title');
+echo $StrongPrime;
+endblock();
+
+
+startblock('content');
+echo'
+	<center>
+	<form method="POST">
+	<br>
+	'.$StrongPrime.'
+	<br>
+	<input type="text" style="width:20%;" class="input" name="N" placeholder="N">
+	<br>
+	<input type="submit" style="width:10%;" class="button" name = "Send" value="'.$Calculate.'">
+	</form>
+	<br>
+	'.$Results.'
+	</center>
+	<div class="ResultBox">
+	'.$result.'
+	</div>';
+
+endblock();
+
+?>
